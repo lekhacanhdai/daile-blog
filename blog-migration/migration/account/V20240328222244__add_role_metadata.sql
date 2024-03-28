04d7e604c5a9f7c0caafc0112b75f573fe7ec3c7
Le Khac Anh Dai <khacdai0801@gmail.com>
2024-03-28 22:35:14 +0700

    init role default

---
+++ blog-migration/migration/account/V20240328222244__add_role_metadata.sql
@@ -0,0 +1,4 @@
+set search_path to blogaccount;
+
+insert into roles (role, description, status) values ('ADMIN', 'admin', 0);
+insert into roles (role, description, status) values ('USER', 'user', 0);
