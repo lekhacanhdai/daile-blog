79f1c580e18c44df8a9c13d3da8ece34dba5bae1
Le Khac Anh Dai <khacdai0801@gmail.com>
2024-04-03 22:59:14 +0700

    Add token claim

--- blog-auth/src/main/java/com/blog/auth/domain/repository/RoleRepository.java
+++ blog-auth/src/main/java/com/blog/auth/domain/repository/RoleRepository.java
@@ -2,8 +2,11 @@ package com.blog.auth.domain.repository;
 
 import com.blog.auth.domain.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -13,4 +16,7 @@ import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
+    @Query("SELECT r FROM RoleEntity r " +
+            "WHERE EXISTS (SELECT 1 FROM UserRoleEntity ur WHERE ur.role.roleId = r.roleId AND ur.user.userId = :userId)")
+    List<RoleEntity> findAllByUserId(@Param("userId") UUID userId);
 }
