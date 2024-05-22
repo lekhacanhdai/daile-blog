97b0500e4ebe2608dc04257b2bef1f94306cb0f6
daile <khacdai0801@gmail.com>
2024-05-22 23:54:20 +0700

    feat: implement debezium connector https://dailekhac0812.atlassian.net/wiki/spaces/KB/pages/33485/Debezium

---
+++ blog-migration/migration/auth/V20240522214836__add_cdc_users.sql
@@ -0,0 +1,12 @@
+create table users (
+   user_id uuid primary key,
+   username varchar(255) not null,
+   password text not null,
+   email varchar(255) not null,
+   status integer not null default 0,
+   full_name varchar(255),
+   created_by uuid,
+   created_date timestamp default now(),
+   modified_by uuid,
+   modified_date timestamp default now()
+);
