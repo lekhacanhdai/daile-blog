b620c3d9e6dfb042a6935373e6c7b6f4cee276f0
Dai Le <dai.le@mesoneer.io>
2024-11-24 12:12:08 +0700

    Verify quantity code Job

--- blog-account/src/main/java/com/blog/account/config/PasswordEncoderConfiguration.java
+++ blog-account/src/main/java/com/blog/account/config/PasswordEncoderConfiguration.java
@@ -11,6 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class PasswordEncoderConfiguration {
+  
+  
+  
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
