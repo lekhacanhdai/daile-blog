18da960720da5871ffaa1c2c162f1f803a9cc72e
Dai Le <khacdai0801@gmail.com>
2024-11-30 13:10:11 +0700

    feat: Integrate sonar

--- blog-common/src/main/java/com/blog/common/kafka/cdc/CdcEventOp.java
+++ blog-common/src/main/java/com/blog/common/kafka/cdc/CdcEventOp.java
@@ -1,9 +1,13 @@
 package com.blog.common.kafka.cdc;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * @author daile
  * @since 08/06/2024
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CdcEventOp {
   public static final String UPDATE = "u";
   public static final String CREATE = "c";
